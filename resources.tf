ae1bc7ba303f2b8ddb26ee784876e7df88d24990
Gustavo Araújo <gustavoaraujo.dev@gmail.com>
2022-11-21 23:17:10 -0300

    feature(resources): add a cluster with one node

---
+++ resources.tf
@@ -0,0 +1,16 @@
+resource "digitalocean_kubernetes_cluster" "kubernetes_cluster" {
+  name    = var.k8s_clustername
+  region  = var.region
+  version = var.k8s_version
+
+  tags = ["k8s"]
+
+  # This default node pool is mandatory
+  node_pool {
+    name       = var.k8s_poolname
+    size       = "s-2vcpu-2gb"
+    auto_scale = false
+    node_count = 1
+    tags       = ["node-pool-tag"]
+  }
+}
