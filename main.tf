b60113f6ec90f5a5b767b0024daaddf883251ab6
Gustavo Araújo <gustavoaraujo.dev@gmail.com>
2022-11-21 23:03:10 -0300

    feature(domain): set domain

--- main.tf
+++ main.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     digitalocean = {
       source = "digitalocean/digitalocean"
-      version = "~> 2.0"
+      version = "~> 2.21.0"
     }
   }
+}
+
+provider "digitalocean"{
+  token = var.do_token
 }
