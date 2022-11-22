76c62553e8be70c07ff3b382141b16d18f35c0d8
Gustavo Araújo <gustavoaraujo.dev@gmail.com>
2022-11-22 01:39:45 -0300

    feature(domain): create record domain

--- domain.tf
+++ domain.tf
@@ -1,3 +1,11 @@
 resource "digitalocean_domain" "domain" {
   name = var.domain_name
 }
+
+resource "digitalocean_record" "main_record" {
+  depends_on = [data.kubernetes_service_v1.data_svc]
+  domain = digitalocean_domain.domain.id
+  type = "A"
+  name = "@"
+  value = data.kubernetes_service_v1.data_svc.status.0.load_balancer.0.ingress.0.ip
+}
